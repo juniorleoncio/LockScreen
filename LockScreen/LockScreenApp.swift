5994d2b8d19d219e4d403f636128f08557cb7479
Junior Leoncio <graciano.leoncio@gmail.com>
2024-02-05 14:33:58 -0300

    Initial Commit

---
+++ LockScreen/LockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenApp.swift
+//  LockScreen
+//
+//  Created by Junior Leoncio on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LockScreenUITests/LockScreenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LockScreenUITestsLaunchTests.swift
+//  LockScreenUITests
+//
+//  Created by Junior Leoncio on 05/02/24.
+//
+
+import XCTest
+
+final class LockScreenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
